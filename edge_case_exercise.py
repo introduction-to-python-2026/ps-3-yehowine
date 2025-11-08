f95696cfdacfa80be2f2e5267aa1657a798c191b
yehowine <jonatannheh@gmail.com>
2025-11-08 16:18:22 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,12 +1,10 @@
 def move(my_list, direction=None):
-    
-    try:
+
         index_of_one = my_list.index(1)
     except ValueError:
         print("Error: The integer 1 was not found in the list.")
         return my_list
 
-   
     if direction == "right" and index_of_one != len(my_list) - 1:
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
@@ -14,8 +12,7 @@ def move(my_list, direction=None):
     elif direction == "left" and index_of_one != 0:
         my_list[index_of_one] = 0
         my_list[index_of_one - 1] = 1
-        
-    
+
     elif direction is None:
         print("No direction specified; list remains unchanged.")
         
